0ccf0ce71dd265468ca6a7382c330b72ff8691de
AlexJurado0 <alex45360981@gmail.com>
2024-11-05 21:22:56 -0300

    comitt alex

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-const Clases = require('./clases.js')
+const { Turno } = require('./clases.js')
 
 function testCliente(){
     const miTurno = new Turno("lunes",2,true)
-    console.log(miTurno.dia == 'lunes');
-    console.log(miTurno.hora == 2);
+    console.log(miTurno.dia == 'lunes')
+    console.log(miTurno.hora == 2)
     console.log(miTurno.libre == true)
 
 }
