61d9722d6b2875c59c85089e93afc14f23579bf2
Gisella-Domizi <giselladomizi72@gmail.com>
2024-11-05 21:23:14 -0300

    CommitGisella

--- test.js
+++ test.js
@@ -1,11 +1,10 @@
-const Clases = require('./clases.js')
+const { Turno } = require('./clases.js');
 
 function testCliente(){
     const miTurno = new Turno("lunes",2,true)
     console.log(miTurno.dia == 'lunes');
     console.log(miTurno.hora == 2);
     console.log(miTurno.libre == true)
-
 }
 
 testCliente()
