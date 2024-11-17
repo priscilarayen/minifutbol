92487ac6304f9ae928f96c10c52c7a478b49ae0d
Samuel94 <samuu_1994@hotmail.com>
2024-11-17 19:30:40 -0300

    modified

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 const express = require('express')
-const path = require('path')
+
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'ejs');
-
 app.get ('/',(req, res)=>{
-    //res.send('<h1>Hola desde servidor Node</p>')
-    res.render('index.ejs',{cohorte : "2023"})
+    res.send('<h1>Hola desde servidor Node</p>')
 })
 
 
 const PORT = 3000
-app.listen(PORT, ()=>{console.log(`Escuchando en el puerto  ${PORT} `)})
+app.listen(PORT, ()=>{console.log(`Escuchando en el puerto ${PORT}`)})
