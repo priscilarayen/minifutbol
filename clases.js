6c31f1dd90d6dd765428ef6fbf5a47cd02d9d002
gabrielbarboza19 <gabrielbarboza21040512@outlook.es>
2024-11-26 20:42:40 -0300

    Mi commit

--- clases.js
+++ clases.js
@@ -1,5 +1,10 @@
-// Archivo de clases
+// clases.js
+class Turno {
+    constructor(dia, hora, libre) {
+        this.dia = dia;
+        this.hora = hora;
+        this.libre = libre;
+    }
+}
 
-
-
-module.exports = {}
+module.exports = { Turno };
